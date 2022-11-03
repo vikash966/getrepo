b215fb4c22aabbacdc575fe7bd514d59aedfb8e7
vikashkumarvikash99@gmail.com <>
2022-11-03 15:44:33 +0530

    intial version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,8 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world! i am here");
     }
 }
